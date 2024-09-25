1b0602bb4243aab67392324f7e8f21d54eef27aa
Lara Kanj <lara.kanj@kcl.ac.uk>
2024-09-25 18:30:35 +0100

    modify the code to print the frequency

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,25 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    
+    list_one = []
+    list_two = []
+    
+    list_one.append(items[0])
+    items.remove(items[0])
+    for item in items:
+        if item in list_one:
+            list_one.append(item)
+        else:
+            list_two.append(item)
+
+    frequencies[list_one[0]] = len(list_one)
+    frequencies[list_two[0]] = len(list_two)
+
+       
     return frequencies
+
+if __name__ == "__main__":
+    item_list = ["banana", "apple", "apple", "banana"]
+    print(frequencies(item_list))
+
